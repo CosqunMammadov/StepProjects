45750f4837b09f81f3ba3a2501eae0afdd8b6618
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-07-09 15:54:21 +0400

    Initial commit

---
+++ happyFamily2/Main.java
@@ -0,0 +1,30 @@
+package happyFamily.happyFamily2;
+
+public class Main {
+    enum testEnum{
+        XL, XXL
+    }
+    public static void main(String[] args) {
+
+        Pet dog= new Pet("dog","Rock",5,75, "eat", "drink", "sleep");
+
+        Human father= new Human();
+        father.name= "Vito";
+        father.surname= "Karleone";
+
+        Human mother= new Human();
+        mother.name= "Jane";
+        mother.surname= "Karleone";
+
+
+
+        Human michael= new Human("Michael","Karleone", 1977, 90);
+        michael.schedule= new String[][]{{"day", "task"}, {"day2", "task2"}};
+
+
+
+
+        System.out.println(dog.toString());
+        System.out.println(michael.toString());
+    }
+}
