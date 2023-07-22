d45b598a658e5670263c296403d0311fac81bb23
CosqunMammadov <cosqun.mammadov23@gmail.com>
2023-07-22 19:35:48 +0400

    gghh

--- readme2/Main.java
+++ readme2/Main.java
@@ -4,15 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Species dog = Species.DOG;
-
         Pet rock = new Pet(dog, "Rock", 5, 75, "eat", "drink", "sleep");
-
         Human father = new Human();
-
         Human mother = new Human();
-
         Human michael = new Human("Michael", "Karleone", 1977, 90, new String[][]{{DayofWeek.MONDAY.name(), "task"}, {DayofWeek.TUESDAY.name(), "task2"}});
-
         Family family = new Family(mother, father, new Human[]{michael}, rock);
         michael.setFamily(family);
 
